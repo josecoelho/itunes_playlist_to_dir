82d99126fd26cf5162307d15d81d8274236e833d
José Coelho <contato@josecoelho.com>
2014-05-29 18:00:10 -0300

    added the option to change the format to copy file

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Itunes Playlist to Dir
 
+This is as simple python script to organize mp3 files from itunes on a folder. I use this to create CDs/pendrives of MP3 to be used on my car...
+
+Its simple, quickly writed and the code is a good peace of junk...
 
 ## What it do?
 
 * copy files described on playlist from itunes to a dir
 * default configuration of itunes is <artist>/<album>/<music>
-* the target will receive <artist>-<album>/<music>,
+* the default copyToFormat value is <artist>/<album>/<track_number> - <name>
 * and the order that items will be copied will be the natural order (alphabetical) of the folders because the order used on some MP3 players is by modified date
+
+
 
 
 ## How to use?
 
-This is a command line tool...
+This is a command line tool tested on MacOS X...
 
 You will need to:
 
@@ -26,5 +31,10 @@ You will need to:
 * Execute the command on the root folder of project
 
 ```
-  python ./itunes_playlist_to_dir.py ~/playlist.xml <target_folder>
+  python <fullpath_to>/itunes_playlist_to_dir.py /home/<user>/playlist.xml <target_folder> "<optional: cotyToFormat>"
+```
+
+E.G.:
+```
+ python /Users/josecoelho/itunes_playlist_to_dir/itunes_playlist_to_dir.py Tarantino.xml ./ "<album>/<track_number> - <name>"
 ```
--- itunes_playlist_to_dir.py
+++ itunes_playlist_to_dir.py
@@ -9,51 +9,86 @@ from shutil import copyfile
 import urllib2
 import os
 
-def itunes_playlist_to_dir(playlist,toDir):
+def itunes_playlist_to_dir(playlist,toDir,copyToFormat):
     '''
         copy files described on playlist from itunes to a dir
-        
+
         default configuration of itunes is <artist>/<album>/<music>
-        the target will receive <artist>-<album>/<music>, 
-        and the order that items will be copied will be the natural order (alphabetical) of the folders 
+        the default copyToFormat value is <artist>/<album>/<track_number> - <name>
+
+        and the order that items will be copied will be the natural order (alphabetical) of the folders
          because the order used on the MP3 player is by modified date
 
     '''
     toCopy = []
-    xml = ET.parse(playlist)    
-    
+    xml = ET.parse(playlist)
+
+    if(not copyToFormat):
+        copyToFormat = "<artist>/<album>/<track_number> - <name>"
+
     nextIsLocation = False
+    nextIsArtist = False
+    nextIsAlbum = False
+    nextIsTrackNumber = False
+    nextIsName = False
     for item in xml.getroot().find('dict').find('dict').findall('dict'):
         #loading items on toCopy
+        metadata = {}
         for i in item.getiterator():
+            if(nextIsArtist):
+                metadata['artist'] = i.text
+            if(nextIsAlbum):
+                metadata['album'] = i.text
+            if(nextIsTrackNumber):
+                metadata['track_number'] = i.text
+            if(nextIsName):
+                metadata['name'] = i.text.replace("/","-")
             if(nextIsLocation):
+                print metadata
+
+                # if artist not found set as undefined
+                if not 'artist' in metadata.keys():
+                    metadata['artist'] = 'undefined'
+                # if album not found set as undefined
+                if not 'album' in metadata.keys():
+                    metadata['album'] = 'undefined'
+                if not 'name' in metadata.keys():
+                    metadata['name'] = 'undefined'
+                # if track_number not defined, then set as 0
+                if not 'track_number' in metadata.keys():
+                    metadata['track_number'] = '0'
+
                 nextIsLocation = False
-                
-                fromF= urllib2.unquote( i.text[i.text.find('/Users'):] )
-                toF= toDir+fromF[fromF.find('Media/')+5:]
-                
-                #changing from Artist/Album to Artist - Album. To do that, we need the penultimate '/' index from string
-                toFArtistSeparatorIdx = toF.rfind('/',0,toF.rfind('/')-1)
-                
-                toTemp = list(toF)
-                toTemp[toFArtistSeparatorIdx] = '-'
-                
-                toF = ''.join(toTemp)
-                
-                
-                
+
+                fromF= urllib2.unquote( i.text )
+                fromF = fromF.replace("file://localhost", '')
+
+                extension = os.path.splitext(fromF)[1]
+
+                toF = toDir+copyToFormat
+
+                toF = toF.replace('<artist>',metadata['artist'])
+                toF = toF.replace('<album>',metadata['album'])
+                toF = toF.replace('<track_number>',metadata['track_number'])
+                toF = toF.replace('<name>',metadata['name'])
+
+                toF = toF+extension
+
                 #ignore, if that path already exists on toDir
                 if os.path.exists(toF):
                     continue
-                
-                
+
                 toCopy.append({'fromF':fromF,'toF':toF})
-            
+
+            nextIsArtist = i.text == 'Artist'
+            nextIsAlbum = i.text == 'Album'
+            nextIsTrackNumber = i.text == 'Track Number'
+            nextIsName = i.text == 'Name'
             nextIsLocation = i.text == 'Location'
-        
+
     #organize list
     toCopy.sort()
-    
+
 
 
     #copy files
@@ -64,15 +99,21 @@ def itunes_playlist_to_dir(playlist,toDir):
         dir = os.path.dirname(toF)
         if not os.path.exists(dir): #create dir on toDir if that not exists
             os.makedirs(dir)
-        
+
         print "Copy {0} to {1}".format(fromF,toF)
         copyfile(fromF,toF)
 
 
 if __name__ == '__main__':
-    itunes_playlist_to_dir(sys.argv[1],sys.argv[2])
+    playlist = sys.argv[1]
+    toDir = sys.argv[2]
+    copyToFormat = ''
+    if(3 < len(sys.argv)):
+      copyToFormat = sys.argv[3]
+
+    itunes_playlist_to_dir(playlist,toDir,copyToFormat)
 
 
 
-    
+
 
